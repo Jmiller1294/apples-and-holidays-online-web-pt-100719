8bc9eac605287db9a74aefb1834be21e7f98242d
Justin Miller <jayman1294@gmail.com>
2019-10-21 00:41:35 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -17,9 +17,8 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #     :memorial_day => ["BBQ"]
   #   }
   # }
-  second_supply_for_fourth_of_july(holiday_supplies)
-  holiday_supplies[:summer][:fourth_of_july][1]
-  holiday_supplies
+  
+  
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
